2f9bf2e0d795e93a16c79223e4f0da4dac742711
jesus jimenez <jesus@jesuss-MacBook-Pro.local>
2023-12-02 10:13:01 -0300

    server side form validation

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,8 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
+import { notFound } from 'next/navigation';
+
 type Params = {
   params: {
     id: string
@@ -14,6 +16,10 @@ export default async function Page({ params }: Params) {
     fetchInvoiceById(id),
     fetchCustomers()
   ])
+
+  if (!invoice) {
+    notFound()
+  }
 
   return (
     <main>
